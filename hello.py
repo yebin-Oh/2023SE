5b6b42de8e1d8bbe15b03599cfae3c4a764da82d
yebin-Oh <84893679+yebin-Oh@users.noreply.github.com>
2023-06-01 15:13:08 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+print("Hello World!!!! maerong maerong maerong maerong maerong ddiririririri")
+
+=======
+print("Hello World!!!! maerong maerong maerong maerong maerong ddiririririri")
+
+>>>>>>> c6308c35efc996de04db8451708d16a108b90ffa
